86b1150a60568dd827091b0f02a6656b06900dce
Dieu Long <dieuhoangphilongit@gmail.com>
2023-01-10 13:24:51 +0700

    build web app

---
+++ android/app/src/main/kotlin/com/example/save_money_web/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.save_money_web
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:save_money_web/home_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'iSaveMoney',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: const HomeScreen(),
+    );
+  }
+}
